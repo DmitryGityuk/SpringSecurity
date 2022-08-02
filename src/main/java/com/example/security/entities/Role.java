a7e4ff2c2c20e19e282332d9d9a59a7ddbe0c8d0
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-08-02 22:16:27 +0500

    added Role

---
+++ src/main/java/com/example/security/entities/Role.java
@@ -0,0 +1,15 @@
+package com.example.security.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+}
