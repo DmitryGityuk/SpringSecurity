ac25a58f04215b0bf842cb694d69f202343830db
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-08-02 22:22:18 +0500

    changed pageForAuthenticatedUsers

--- src/main/java/com/example/security/controllers/MainController.java
+++ src/main/java/com/example/security/controllers/MainController.java
@@ -1,7 +1,8 @@
 package com.example.security.controllers;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import com.example.security.entities.User;
+import com.example.security.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,24 +10,31 @@ import java.security.Principal;
 
 @RestController
 public class MainController {
+    private UserService service;
+
+    @Autowired
+    public void setService(UserService service) {
+        this.service = service;
+    }
+
     @GetMapping("/")
-    public String homePage(){
+    public String homePage() {
         return "home";
     }
 
     @GetMapping("/authenticated")
-    public String pageForAuthenticatedUsers(Principal principal){
-        Authentication a = SecurityContextHolder.getContext().getAuthentication();
-        return "secured part of web service: " + principal.getName();
+    public String pageForAuthenticatedUsers(Principal principal) {
+        User user = service.findByUsername(principal.getName());
+        return "secured part of web service: " + user.getUsername() + " " + user.getEmail();
     }
 
     @GetMapping("/read_profile")
-    public String pageForReadProfile(){
+    public String pageForReadProfile() {
         return "this is a read profile page";
     }
 
     @GetMapping("/only_for_admins")
-    public String pageOnlyForAdmins(){
+    public String pageOnlyForAdmins() {
         return "this is a admins page";
     }
 }
